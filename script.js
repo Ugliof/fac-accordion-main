36c6bb59926a873f97986046d66d474ad2fb320d
Uglio Fergutz <ugliofergutz@gmail.com>
2025-12-29 22:43:27 -0300

    Andamento - Organizando o style

---
+++ script.js
@@ -0,0 +1,8 @@
+let p1 = document.querySelectorAll('per1')
+let p2 = document.querySelectorAll('per2')
+let p3 = document.querySelectorAll('per3')
+let p4 = document.querySelectorAll('per4')
+
+function mostrar() {
+    if (p1)
+}
